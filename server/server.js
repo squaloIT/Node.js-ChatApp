4e0a2b9da5a0b98bc712044b40a4a8e0771df469
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2018-08-04 20:28:34 +0200

    Dodat start za heroku u package.json

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const path = require("path");
 const publicPath = path.join(__dirname, "../public");
-
 const express = require("express");
 var app = express();
-// process.env.PORT=3000;
+
 var middleware = require('./middleware/middleware');
+
+app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
 
-app.get("/", middleware, (req,res)=>{
-    var porukica = req.novDeo;
-    console.log(porukica);
-    res.sendFile(publicPath+'/index.html');
-});
+// app.get("/", middleware, (req,res)=>{
+//     var porukica = req.novDeo;
+//     console.log(porukica);
+//     res.sendFile(publicPath+'/index.html');
+// });
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);

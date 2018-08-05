fb85031f4ce2a6c11fa1c1579403490c8dbc00aa
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2018-08-05 19:01:03 +0200

    Dodati connect i disconnect event handleri

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
 const path = require("path");
-const publicPath = path.join(__dirname, "../public");
 const express = require("express");
-var app = express();
+const http = require("http");
+const socketIO = require("socket.io");
 
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "../public");
+
+var app = express();
+var serverHTTP = http.createServer(app);
+var io = socketIO(serverHTTP);
 var middleware = require('./middleware/middleware');
 
 app.use(express.static(publicPath));
 
-const port = process.env.PORT || 3000;
-
+io.on('connection',(socket)=>{
+  console.log('Novi korisnik se prikljucio');
+  // console.log(socket);
+  socket.on("disconnect",()=>{
+    console.log('Korisnik je izasao');
+  });
+});
+// io.on('disconnection',()=>{
+//   console.log('Korisnik je izasao');
+// });
 // app.get("/", middleware, (req,res)=>{
 //     var porukica = req.novDeo;
 //     console.log(porukica);
 //     res.sendFile(publicPath+'/index.html');
 // });
 
-app.listen(port, () => {
+serverHTTP.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
   
